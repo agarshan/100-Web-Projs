c18f8a6b0190e75431d3be0339d852a9ea7d4fe1
agarshan senthamilpalan <agarshan.senthamipalan@infor.com>
2025-03-06 22:27:38 +0530

    add home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
-import { Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <Button>Hello</Button>
+      <Box minH={"100vh"}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreatePage />} />
+        </Routes>
+      </Box>
     </>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
